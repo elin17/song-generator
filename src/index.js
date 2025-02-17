1d184740a655f1cafb5aa137b3fafc6006fe80d4
elin17 <elin.guinchoma@gmail.com>
2025-02-17 13:21:21 +1100

    Added basic form handling and Typewriter plugin.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateSong(event) {
+  event.preventDefault();
 
+  new Typewriter("#song", {
+    strings: "The song will go here",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let songFormElement = document.querySelector("#song-generator-form");
+songFormElement.addEventListener("submit", generateSong);
